990ee64bb8cd5e265f50895b79690a7cee259fbf
User <User@CORP-AUD-NUM>
2024-12-13 13:54:32 +0300

    Добавьте файлы проекта.

---
+++ SamRabGaynutdinov2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SamRabGaynutdinov2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
